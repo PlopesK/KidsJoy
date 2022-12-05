4d13c8f8188870a942047b9512e085861ee4391f
PlopesK <popi081004@hotmail.com>
2022-12-05 18:49:44 -0300

    Used the Anemo System site as an base, since I'm nearly at the due of this homework lol

---
+++ jsquery.js
@@ -0,0 +1,49 @@
+/* Navbar */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+} 
+
+/* Navbar Mobile */
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+/* Reveal text */
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+/* Loading */
+$(window).on("load", function(){
+  $(".load-wrapper").fadeOut("slow");
+});
